2084073e542f4edec97d4a147618f4774fd0c62f
SergeyL <mkru@mail.ru>
2023-02-09 14:56:24 +0300

    Added hw7

---
+++ hw7_py/phone_book_main.py
@@ -0,0 +1,13 @@
+# Реализовать свой проект на каком-то другом языке программирования
+
+from controller.manage_phone_book import ManagePhoneBook
+
+class PhoneBookMain:
+    @staticmethod
+    def main():
+        manage_phone_book = ManagePhoneBook()
+        manage_phone_book.start()
+
+
+if __name__ == "__main__":
+    PhoneBookMain.main()
